69836227d8c21dfbd149ac36e4b169c459ad90fa
redbrick1357 <feng.sian@gmail.com>
2017-03-30 08:50:33 -0300

    initial commit (manual upload)

---
+++ src/main/java/pers/fengsian/gettingStarted.java
@@ -0,0 +1,10 @@
+package pers.fengsian;
+
+/**
+ * Created by cfeng01 on 30/03/17.
+ */
+public class gettingStarted {
+    public void Main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
